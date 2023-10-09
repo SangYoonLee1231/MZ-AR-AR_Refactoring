04000003505b64b3cb11aedfd10a749b52544d31
SangYoonLee <prestro1231@hufs.ac.kr>
2023-10-10 00:32:59 +0900

    Modify: 이벤트 페이지 디자인 수정 완료

--- src/pages/Event/EventPage.jsx
+++ src/pages/Event/EventPage.jsx
@@ -5,10 +5,22 @@ import "./EventPage.scss";
 import Header from "../../components/header.jsx";
 
 const event = () => {
+  const eventList = [
+    ["프라다 할인", "11.30"],
+    ["~~ 할인", "11.30"],
+    ["~~ 할인", "11.30"],
+  ];
+
   return (
     <div>
       <Header />
+      <div className="SizedBox"></div>
+      <div className="SizedBox"></div>
       <div className="center">
+        <div className="event-page-phrase">
+          <span>분당점만의</span>
+          <span>이벤트들을 모아봤어요!</span>
+        </div>
         <div className="SizedBox"></div>
 
         <Link to="/m">
@@ -18,11 +30,21 @@ const event = () => {
         </Link>
 
         <div className="SizedBox"></div>
+        <div className="SizedBox"></div>
 
-        {[1, 2, 3, 4, 5].map((id) => {
-          return <div key={id} className="ad-style"></div>;
+        {eventList.map((eventInfo) => {
+          return (
+            <div className="ad-this-style">
+              <span className="event-name">{eventInfo[0]}</span>
+              <span>~{eventInfo[1]}</span>
+            </div>
+          );
         })}
       </div>
+      <div className="SizedBox"></div>
+      <div className="SizedBox"></div>
+      <div className="SizedBox"></div>
+      <div className="SizedBox"></div>
       <Nav />
     </div>
   );
