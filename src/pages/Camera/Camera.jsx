73f87f8234d7d637aeb200803c038bb2682f0cb7
leestana01 <leestana01@naver.com>
2023-10-09 22:50:25 +0900

    [FIX] AR 기능 수정

--- src/pages/Camera/Camera.jsx
+++ src/pages/Camera/Camera.jsx
@@ -1,157 +1,111 @@
-import React, { useRef, useEffect, useState } from 'react';
-import Webcam from 'react-webcam';
-import './Camera.scss';
-import DraggableDiv from '../DraggableDiv.jsx';
+import React, { useEffect, useRef, useState } from 'react';
 
-const Camera = () => {
-    const webcamRef = useRef(null);
-    const [circlePosition, setCirclePosition] = useState({ x: 50, y: 50 });
-    const [circleSize, setCircleSize] = useState(50);
-    const [rotationAngle, setRotationAngle] = useState(0); //살짝 회전해가면서 볼 수 있게 기능 추가
+
+function Camera() {
+    const videoRef = useRef(null);
+    const canvasRef = useRef(null);
+    const [imgPos, setImgPos] = useState({ x: 0, y: 0 });
+    const [imgSize, setImgSize] = useState(100); // 슬라이더로 조절할 이미지 크기, 퍼센트 단위
+    const [rotation, setRotation] = useState(0); // 이미지 회전 각도
 
     useEffect(() => {
-        if (webcamRef.current) {
-            navigator.mediaDevices
-                .getUserMedia({ video: true })
-                .then((stream) => {
-                    webcamRef.current.srcObject = stream;
-                })
-                .catch((error) => {
-                    console.error('카메라 열기 실패:', error);
-                });
-        }
+        // 웹캠 설정
+        navigator.mediaDevices.getUserMedia({ video: true })
+        .then((stream) => {
+            const video = videoRef.current;
+            video.srcObject = stream;
+            video.play();
+        });
     }, []);
 
-    //사진을 촬영해서 새 창에 띄운다
-    const capture = () => {
-        const imageSrc = webcamRef.current.getScreenshot();
-
-        var newWindow = window.open("", "_blank");
-
-        if (newWindow) {
-            newWindow.document.write(`
-            <html>
-            <body>
-                <img src="${imageSrc}" alt="이미지">
-                <button id="backButton">다시 착용하기</button>
-            
-            </body>
-            </html>
-        `);
-
-            var backButton = newWindow.document.getElementById("backButton");
-            backButton.addEventListener("click", () => {
-                newWindow.close(); //새 창을 닫고
-                window.focus(); //원래 창으로 포커스를 돌려줌
+    const handleMouseDown = (e) => {
+        const clientX = e.clientX || e.touches[0].clientX;
+        const clientY = e.clientY || e.touches[0].clientY;
+        const offsetX = clientX - imgPos.x;
+        const offsetY = clientY - imgPos.y;
+    
+        const handleMouseMove = (e) => {
+            const moveX = e.clientX || e.touches[0].clientX;
+            const moveY = e.clientY || e.touches[0].clientY;
+            console.log(`moveX: ${moveX}, moveY: ${moveY}`);  // 현재 위치 로깅
+            setImgPos({
+                x: moveX - offsetX,
+                y: moveY - offsetY,
             });
-
-
-        } else {
-            alert("팝업 창이 차단되었습니다. 브라우저 설정에서 팝업 차단을 해제해주세요.");
-        }
+        };
+    
+        const handleMouseUp = () => {
+            document.removeEventListener('mousemove', handleMouseMove);
+            document.removeEventListener('mouseup', handleMouseUp);
+            document.removeEventListener('touchmove', handleMouseMove);
+            document.removeEventListener('touchend', handleMouseUp);
+        };
+    
+        document.addEventListener('mousemove', handleMouseMove);
+        document.addEventListener('mouseup', handleMouseUp);
+        document.addEventListener('touchmove', handleMouseMove);
+        document.addEventListener('touchend', handleMouseUp);
     };
 
-    const rotateLeft = () => {
-        setRotationAngle((prevAngle) => prevAngle - 20);
-    };
+    const capture = () => {
+        const videoElement = videoRef.current;
+        const canvasElement = canvasRef.current;
+        const ctx = canvasElement.getContext('2d');
 
-    const rotateRight = () => {
-        setRotationAngle((prevAngle) => prevAngle + 20);
-    };
+        // 웹캠 캡처
+        ctx.drawImage(videoElement, 0, 0, videoElement.width, videoElement.height);
 
+        // 이미지 로드 및 캡처
+        const img = new Image();
+        img.src = '/images/images-2.jpg';
+        img.onload = () => {
+            // 이미지 회전과 위치 조절
+            ctx.save();
+            ctx.translate(imgPos.x + (img.width * imgSize / 200), imgPos.y + (img.height * imgSize / 200));
+            ctx.rotate(rotation * (Math.PI / 180));
+            ctx.drawImage(img, -(img.width * imgSize / 200), -(img.height * imgSize / 200), img.width * (imgSize / 100), img.height * (imgSize / 100));
+            ctx.restore();
 
-    //작동 안함^^.. 그레서 일단 line과 circle 화면에서 삭제함
-    const moveCircle = (direction) => {
-        const stepSize = 5; //버튼 한번 클릭 시 5만큼 이동하게 함
-        const lineStartX = 0; //선분의 시작 x 좌표
-        const lineEndX = 300; //선분의 끝 x 좌표
-        const circleWidth = circleSize; //원의 지름
+            // // 캡처된 이미지를 새 창에 띄우기
+            // const dataURL = canvasElement.toDataURL('image/png');
+            // window.open(dataURL, '_blank');
+            // 캡처된 이미지를 새 창에 띄우기 대신 페이지에 삽입
+            const dataURL = canvasElement.toDataURL('image/png');
+            const imgTag = document.createElement('img');
+            imgTag.src = dataURL;
+            document.body.appendChild(imgTag);
 
-        if (direction === 'left') {
-            //원의 위치를 stepSize만큼 왼쪽으로 이동하되 선분의 왼쪽 끝을 넘어가지 않도록 제한
-            const newX = Math.max(lineStartX + circleWidth / 2, circlePosition.x - stepSize);
-            setCirclePosition((prevPosition) => ({
-                ...prevPosition,
-                x: newX,
-            }));
-            //원의 크기를 stepSize만큼 축소
-            setCircleSize((prevSize) => Math.max(prevSize - stepSize, 20));
-        } else if (direction === 'right') {
-            //원의 위치를 stepSize만큼 오른쪽으로 이동하되 선분의 오른쪽 끝을 넘어가지 않도록 제한
-            const newX = Math.min(lineEndX - circleWidth / 2, circlePosition.x + stepSize);
-            setCirclePosition((prevPosition) => ({
-                ...prevPosition,
-                x: newX,
-            }));
-            //원의 크기를 stepSize만큼 증가
-            setCircleSize((prevSize) => Math.min(prevSize + stepSize, 200));
-        }
+        };
     };
 
     return (
-        <div className="camera-container center">
-            <div className="SizedBox"></div>
-            <a>AR 이미지 아래에 착용하고자 하는 신체 부위를 위치하신 후 스타일을 확인하세요.</a>
-            <a>하단의 버튼을 눌러 크기와 각도를 조절하고 캡쳐하실 수 있습니다.</a>
-            <a>제품 이미지를 드래그하여 원하는 곳에 위치시킬 수도 있습니다.</a>
-            <div className="SizedBox"></div>
-            <div className="camera-wrapper">
-                <Webcam
-                    audio={false}
-                    ref={webcamRef}
-                    screenshotFormat="image/jpeg"
-                    className="camera"
-                />
-                <div className="overlay">
 
-                    <DraggableDiv>
-                        <img
-                            src="/images/images-2.jpg"
-                            alt="My Image"
-                            className="overlay-image"
-                            style={{
-                                transform: `scale(${circleSize / 100}) rotate(${rotationAngle}deg)`,
-                            }}
-                        />
-                    </DraggableDiv>
-
-                    <div className="overlay-button">
-
-                        <button
-                            className="move-button left camera-button-style"
-                            onClick={() => moveCircle('left')}
-                        >
-                            제품 축소
-                        </button>
-                        <div className="SizedBox"></div>
-
-                        <button
-                            className="move-button right camera-button-style"
-                            onClick={() => moveCircle('right')}
-                        >
-                            제품 확대
-                        </button>
-                        <div className="SizedBox"></div>
-                        <button
-                            className="move-button left camera-button-style"
-                            onClick={rotateLeft}
-                        >
-                            왼쪽 회전
-                        </button>
-                        <div className="SizedBox"></div>
-                        <button
-                            className="move-button right camera-button-style"
-                            onClick={rotateRight}
-                        >
-                            오른쪽 회전
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <div className="SizedBox"></div>
-            <button onClick={capture} className="camera-button-style">사진 찍기</button>
+        <>
+        <video ref={videoRef} width="640" height="480" autoPlay></video>
+        <canvas ref={canvasRef} width="640" height="480" style={{ display: 'none' }}></canvas>
+        <div
+            onMouseDown={handleMouseDown}
+            onTouchStart={handleMouseDown}
+            style={{
+                position: 'absolute',
+                top: imgPos.y,
+                left: imgPos.x,
+                cursor: 'grab',
+                transform: `rotate(${rotation}deg)`,
+            }}
+        >
+            <img src="/images/images-2.jpg" width={`${imgSize}%`} />
         </div>
+        <div>
+            <button onClick={() => setRotation(rotation + 15)}>시계 회전</button>
+            <button onClick={() => setRotation(rotation - 15)}>반시계 회전</button>
+        </div>
+        <div>
+            <input type="range" min="10" max="200" value={imgSize} onChange={(e) => setImgSize(e.target.value)} />
+        </div>
+        <button onClick={capture}>캡처</button>
+        </>
     );
-};
+}
 
 export default Camera;
