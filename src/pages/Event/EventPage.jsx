62f8e965ee087a89caccec7cf9e2271dabdf1c30
SangYoonLee <prestro1231@hufs.ac.kr>
2023-10-10 00:08:22 +0900

    Modify: 이벤트 페이지 수정 중

--- src/pages/Event/EventPage.jsx
+++ src/pages/Event/EventPage.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import Nav from '../../components/Nav.jsx';
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import Nav from "../../components/Nav.jsx";
 import "./EventPage.scss";
-import Header from '../../components/header.jsx';
+import Header from "../../components/header.jsx";
 
 const event = () => {
   return (
@@ -12,14 +12,16 @@ const event = () => {
         <div className="SizedBox"></div>
 
         <Link to="/m">
-          <button className="game-button">B 사이트 연동하고 상품권 받으러 가기</button>
+          <button className="game-button">
+            B 사이트 연동하고 상품권 받으러 가기
+          </button>
         </Link>
 
         <div className="SizedBox"></div>
 
-        {
-          [1, 2, 3, 4, 5].map(() => { return (<div className="ad-style"></div>) })
-        }
+        {[1, 2, 3, 4, 5].map((id) => {
+          return <div key={id} className="ad-style"></div>;
+        })}
       </div>
       <Nav />
     </div>
