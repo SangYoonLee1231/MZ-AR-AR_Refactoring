f98be27b6329d6005360755982f09a2ee298a9be
leestana01 <leestana01@naver.com>
2023-10-10 02:08:45 +0900

    AR 카메라 기능 작업중

--- src/pages/Camera/Camera.jsx
+++ src/pages/Camera/Camera.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-
+import './Camera.scss';
 
 function Camera() {
     const videoRef = useRef(null);
@@ -7,6 +7,17 @@ function Camera() {
     const [imgPos, setImgPos] = useState({ x: 0, y: 0 });
     const [imgSize, setImgSize] = useState(100); // 슬라이더로 조절할 이미지 크기, 퍼센트 단위
     const [rotation, setRotation] = useState(0); // 이미지 회전 각도
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+        
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+    
 
     useEffect(() => {
         // 웹캠 설정
@@ -16,7 +27,7 @@ function Camera() {
             video.srcObject = stream;
             video.play();
         });
-    }, []);
+    }, [windowWidth]);
 
     const handleMouseDown = (e) => {
         const clientX = e.clientX || e.touches[0].clientX;
@@ -27,11 +38,26 @@ function Camera() {
         const handleMouseMove = (e) => {
             const moveX = e.clientX || e.touches[0].clientX;
             const moveY = e.clientY || e.touches[0].clientY;
-            console.log(`moveX: ${moveX}, moveY: ${moveY}`);  // 현재 위치 로깅
-            setImgPos({
-                x: moveX - offsetX,
-                y: moveY - offsetY,
-            });
+        
+            const newPosX = moveX - offsetX;
+            const newPosY = moveY - offsetY;
+        
+            const videoElement = videoRef.current;
+            const videoWidth = videoElement.offsetWidth;
+            const videoHeight = videoElement.offsetHeight;
+        
+            const imgElement = document.querySelector('img');
+            const imgWidth = imgElement.offsetWidth;
+            const imgHeight = imgElement.offsetHeight;
+        
+            if (newPosX >= 0 && newPosX <= videoWidth - imgWidth &&
+                newPosY >= 0 && newPosY <= videoHeight - imgHeight) {
+                console.log(`moveX: ${moveX}, moveY: ${moveY}`);  // 현재 위치 로깅
+                setImgPos({
+                    x: newPosX,
+                    y: newPosY,
+                });
+            }
         };
     
         const handleMouseUp = () => {
@@ -45,6 +71,32 @@ function Camera() {
         document.addEventListener('mouseup', handleMouseUp);
         document.addEventListener('touchmove', handleMouseMove);
         document.addEventListener('touchend', handleMouseUp);
+    };
+
+    // 이미지 크기를 변경할 때의 로직
+    const handleImageSizeChange = (e) => {
+        const newSize = e.target.value;
+        const videoElement = videoRef.current;
+        const videoWidth = videoElement.offsetWidth;
+        const videoHeight = videoElement.offsetHeight;
+    
+        const imgElement = document.querySelector('img');
+        const imgWidth = imgElement.naturalWidth * (newSize / 100);
+        const imgHeight = imgElement.naturalHeight * (newSize / 100);
+    
+        let newX = imgPos.x;
+        let newY = imgPos.y;
+    
+        // 이미지가 웹캠 영역을 벗어나는지 확인하고, 벗어난다면 영역 안으로 재조정
+        if (imgPos.x + imgWidth > videoWidth) {
+        newX = videoWidth - imgWidth;
+        }
+        if (imgPos.y + imgHeight > videoHeight) {
+        newY = videoHeight - imgHeight;
+        }
+    
+        setImgPos({ x: newX, y: newY });
+        setImgSize(newSize);
     };
 
     const capture = () => {
@@ -79,19 +131,18 @@ function Camera() {
     };
 
     return (
-
         <>
-        <video ref={videoRef} width="640" height="480" autoPlay></video>
-        <canvas ref={canvasRef} width="640" height="480" style={{ display: 'none' }}></canvas>
+        <video ref={videoRef} width={windowWidth} height={Math.floor(windowWidth * (3 / 4))} autoPlay></video>
+        <canvas ref={canvasRef} width={windowWidth} height={Math.floor(windowWidth * (3 / 4))} style={{ display: 'none' }}></canvas>
         <div
             onMouseDown={handleMouseDown}
             onTouchStart={handleMouseDown}
             style={{
-                position: 'absolute',
-                top: imgPos.y,
-                left: imgPos.x,
-                cursor: 'grab',
-                transform: `rotate(${rotation}deg)`,
+            position: 'absolute',
+            top: imgPos.y,
+            left: imgPos.x,
+            cursor: 'grab',
+            transform: `rotate(${rotation}deg)`,
             }}
         >
             <img src="/images/images-2.jpg" width={`${imgSize}%`} />
@@ -101,9 +152,9 @@ function Camera() {
             <button onClick={() => setRotation(rotation - 15)}>반시계 회전</button>
         </div>
         <div>
-            <input type="range" min="10" max="200" value={imgSize} onChange={(e) => setImgSize(e.target.value)} />
+            <input type="range" min="70" max="100" value={imgSize} onChange={handleImageSizeChange} />
         </div>
-        <button onClick={capture}>캡처</button>
+        <button className="capture-button" onClick={capture}>캡처</button>
         </>
     );
 }
