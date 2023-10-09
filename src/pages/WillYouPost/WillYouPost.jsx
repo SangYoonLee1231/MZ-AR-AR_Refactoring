4e9710ce12910ab2af503d5aded74405c4d2e50d
SangYoonLee <prestro1231@hufs.ac.kr>
2023-10-09 23:33:19 +0900

    Modify: AR 화면 디자인 수정 (계속 작업중)

--- src/pages/WillYouPost/WillYouPost.jsx
+++ src/pages/WillYouPost/WillYouPost.jsx
@@ -11,28 +11,30 @@ const WillYouPost = () => {
       <div className="SizedBox"></div>
       <div className="body">
         <div className="center">
-          <div className="SizedBox"></div>
-          <h1>
-            <span className="bold">사진을 출품해보세요!</span>
-          </h1>
-          <div className="SizedBox"></div>
-          <h1>
-            <span className="bold">
-              AR 트릭아트 사진 대전에 참가하시겠어요?
-            </span>
-          </h1>
-          <div className="SizedBox"></div>
-          <h1>
-            <span className="bold">좋아요 순위</span>에 따라{" "}
-            <span className="bold">롯데백화점 상품권</span>을 수여합니다!
-          </h1>
-          <div className="SizedBox"></div>
-          <h1>순위에 들지 못해도 괜찮아요!</h1>
-          <h1>
-            순위에 없어도 <span className="bold">추첨</span>에 따라 월간
-            지급까지!
-          </h1>
-          <div className="SizedBox"></div>
+          <div className="text-area">
+            <div className="SizedBox"></div>
+            <h1>
+              <span className="bold title">사진을 출품해보세요!</span>
+            </h1>
+            <div className="SizedBox"></div>
+            <h1>
+              <span className="bold">
+                AR 트릭아트 사진 대전에 참가하시겠어요?
+              </span>
+            </h1>
+            <div className="SizedBox"></div>
+            <h1>
+              <span className="bold">좋아요 순위</span>에 따라{" "}
+              <span className="bold">롯데백화점 상품권</span>을 수여합니다!
+            </h1>
+            <div className="SizedBox"></div>
+            <h1>순위에 들지 못해도 괜찮아요!</h1>
+            <h1>
+              순위에 없어도 <span className="bold">추첨</span>에 따라 월간
+              지급까지!
+            </h1>
+            <div className="SizedBox"></div>
+          </div>
 
           <div className="button-area">
             <Link to="/l">
--- src/pages/WriteTitle/WriteTitle.jsx
+++ src/pages/WriteTitle/WriteTitle.jsx
@@ -1,63 +1,77 @@
-import React, { useRef, useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch, useNavigate } from 'react-router-dom';
-import Header from '../../components/header.jsx';
+import React, { useRef, useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  useNavigate,
+} from "react-router-dom";
+import Header from "../../components/header.jsx";
 
-import './WriteTitle.scss';
+import "./WriteTitle.scss";
 
 function WriteTitle() {
+  const [username, setUsername] = useState("");
 
-    const [username, setUsername] = useState('');
+  const [showToast, setShowToast] = useState(false);
+  const history = useNavigate();
 
-    const [showToast, setShowToast] = useState(false);
-    const history = useNavigate();
+  const LogIn_id = (e) => {
+    setUsername(e.target.value);
+  };
+  const handleLogIn = () => {
+    if (username !== "") {
+      setShowToast(true);
+      setTimeout(() => {
+        setShowToast(false);
+        //contest 페이지로 이동하기
+        history("/i");
+      }, 500); //0.8초 후 토스트 메시지를 숨기고 이동
+    } else {
+      alert("사진에 제목이 등록되었습니다!");
+    }
+  };
 
-    const LogIn_id = (e) => {
-        setUsername(e.target.value);
-    };
-    const handleLogIn = () => {
-        if (username !== '') {
-            setShowToast(true);
-            setTimeout(() => {
-                setShowToast(false);
-                //contest 페이지로 이동하기
-                history('/i');
-            }, 500); //0.8초 후 토스트 메시지를 숨기고 이동
-        } else {
-            alert('사진에 제목이 등록되었습니다!');
-        }
-    };
+  //입력 시 버튼이 활성화되게 하기 위한 변수
+  const isButtonDisabled = username === "";
 
-    //입력 시 버튼이 활성화되게 하기 위한 변수
-    const isButtonDisabled = username === '';
+  return (
+    <div className="vertical-center-lineUp">
+      <Header />
+      <div className="SizedBox_ver1"></div>
+      <span className="bold">사진의 어울리는 제목을 입력해주세요.</span>
+      <div className="SizedBox_ver2"></div>
+      <span>여러분의 창의력을 뽑내도 좋아요!</span>
+      <div className="SizedBox_ver2"></div>
+      <div className="SizedBox_ver2"></div>
+      <div className="input-area">
+        <input
+          type="text"
+          value={username}
+          onChange={LogIn_id}
+          style={{ width: "250px", height: "40px" }}
+          placeholder="사진의 제목을 입력하세요."
+        />
 
-    return (
-        <div className="vertical-center-lineUp">
-
-            <Header />
-            <div className="SizedBox_ver1"></div>
-            <a>사진의 제목을 입력해주세요.</a>
-            <div className="SizedBox_ver2"></div>
-            <input
-                type="text"
-                value={username}
-                onChange={LogIn_id}
-                style={{ width: '300px', height: '40px' }}
-                placeholder="사진의 제목을 입력하세요."
-            />
-
-            <div className="SizedBox_ver2"></div>
-
-            <button className="title-button-style" onClick={handleLogIn} disabled={isButtonDisabled}>
-
-                <a>완료</a>
-
-            </button>
-            <div className="SizedBox_ver2"></div>
-            <a>사회의 미풍양속을 해치지 않는 제목을 입력해주세요.</a>
-            <div className="SizedBox_ver2"></div>
-            {showToast && <div className="toast">사진에 제목이 등록되었습니다!</div>}
-        </div>
-    );
+        <button
+          className="title-button-style"
+          onClick={handleLogIn}
+          disabled={isButtonDisabled}
+        >
+          <a>입력 완료</a>
+        </button>
+      </div>
+      <div className="SizedBox_ver2"></div>
+      <div className="SizedBox_ver2"></div>
+      <span>사회의 미풍양속을 해치지 않는 제목을 입력해주세요.</span>
+      <div className="SizedBox_ver2"></div>
+      <span>성희롱 또는 기타 불쾌감을 조성하는 제목을 사용할 경우</span>
+      <div className="SizedBox_ver2"></div>
+      <span>민령사상 책임을 물을 수 있습니다.</span>
+      <div className="SizedBox_ver2"></div>
+      {showToast && <div className="toast">사진에 제목이 등록되었습니다!</div>}
+    </div>
+  );
 }
 
 export default WriteTitle;
